5b222b495a4c90efc734ed733a2dc2b1a2a126ff
DmytroKorobkoUA <dmytro.korobko@devabit.com>
2024-10-01 14:59:52 +0100

    Feat: implement hash functions

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    const hash = createHash('sha256');
+    const stream = createReadStream(filePath);
+
+    stream.on('data', chunk => hash.update(chunk));
+
+    stream.on('end', () => {
+        console.log(hash.digest('hex'));
+    });
+
+    stream.on('error', () => {
+        throw new Error('FS operation failed');
+    });
 };
 
 await calculateHash();
